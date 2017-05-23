2d96efea6b6ac0cc9d2e281cbcd50a26f6bc8004
zhaosl <zhaosl@linkface.cn>
2017-05-24 00:18:38 +0800

    page 55

--- play2/myplay/app/controllers/Application.scala
+++ play2/myplay/app/controllers/Application.scala
@@ -34,4 +34,33 @@ object Application extends Controller {
   def listProductsAsXML = Action{
     Ok(views.xml.products(productMap))
   }
+
+  //curl -v http://localhost:9000/header_example
+  def modifyHeaders = Action{
+    Ok("Header Modification examples")
+      .withHeaders(
+        play.api.http.HeaderNames.ETAG -> "foo_scala"
+      )
+  }
+  //curl -v http://localhost:9000/cookie_example
+  def modifyCookies = Action{
+    val cookie = Cookie("source", "tw", Some(60*60))
+    Ok("Cookie Modification examples")
+      .withCookies(cookie)
+  }
+
+  def modifySession = Action{ request =>
+    val sessionVar = "user_pref"
+    request.session.get(sessionVar) match{
+      case Some(userPref) => {
+        Ok("Found userPref: %s".format(userPref))
+      }
+      case None =>{
+        Ok("Setting session var %s".format(sessionVar))
+        .withSession(
+          sessionVar -> "tw"
+        )
+      }
+    }
+  }
 }
--- play2/myplay/test/ProductSpec.scala
+++ play2/myplay/test/ProductSpec.scala
@@ -1,12 +1,38 @@
+import models.Product
 import org.specs2.mutable._
+import play.api.test.{FakeRequest, WithApplication}
+import play.api.test.Helpers._
+
+
+
 /**
   * Created by zhaoshoulai on 2017/5/23.
   */
 class ProductSpec extends Specification{
 
-  "The 'product' string" should {
-    "contain seven characters" in {
-      "product" must have size(8)
+  "controller.Application" should {
+    "respond with xml for /product.xml requests" in new WithApplication() {
+      val result = controllers.Application.listProductsAsXML()(FakeRequest())
+      status(result) must equalTo(OK)
+      contentType(result) must beSome("application/xml")
+      contentAsString(result) must contain("products")
     }
   }
+
+  "models.Product" should {
+    "create a product with save()" in new WithApplication() {
+      val product = Product(4, "Apple")
+      val productId = Product.save(product)
+
+      productId must not be None
+    }
+  }
+
+  "The 'product' string" should {
+    "contain seven characters" in {
+      "product" must have size(7)
+    }
+  }
+
+
 }
